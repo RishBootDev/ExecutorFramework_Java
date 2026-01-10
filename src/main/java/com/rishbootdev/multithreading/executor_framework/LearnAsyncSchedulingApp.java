64c8d2c5b009d1472cd3494068759d714cc6331e
Rishabh <rishabhdubey1472004@gmail.com>
2026-01-11 00:23:39 +0530

    first commit

--- src/main/java/com/rishbootdev/multithreading/executor_framework/LearnAsyncSchedulingApp.java
+++ src/main/java/com/rishbootdev/multithreading/executor_framework/LearnAsyncSchedulingApp.java
@@ -3,6 +3,7 @@ package com.rishbootdev.multithreading.executor_framework;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -12,6 +13,49 @@ import java.util.concurrent.TimeUnit;
 public class LearnAsyncSchedulingApp {
 
     public static void main(String[] args) {
+
+        //test1();
+        Scanner sc = new Scanner(System.in);
+        log.info("Please enter the command ");
+        String cmd = sc.next();
+        //test2(cmd);
+        test3(cmd);
+    }
+
+    public static void test2(String cmd) {
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                6,10,2, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(10)
+        );
+
+        log.info("Starting the main thread -> {}",Thread.currentThread().getName());
+
+        threadPoolExecutor.submit(new LongRunningTask(cmd));
+        log.info("Ending the main thread -> {}", Thread.currentThread().getName());
+
+    }
+
+    public static void test3(String cmd) {
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                6,10,2, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(10)
+        );
+
+        log.info("Starting the main thread -> {}",Thread.currentThread().getName());
+
+        for (int i = 0; i < 100; i++) {
+            threadPoolExecutor.submit(new LongRunningTask(cmd));
+        }
+
+        threadPoolExecutor.submit(new LongRunningTask(cmd));
+        log.info("Ending the main thread -> {}", Thread.currentThread().getName());
+
+    }
+
+
+    public static void test1() {
 
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
                 6,10,2, TimeUnit.SECONDS,
@@ -27,7 +71,6 @@ public class LearnAsyncSchedulingApp {
                 }
 
                 log.info("Ending the task");
-
         });
 
         }
--- src/main/java/com/rishbootdev/multithreading/executor_framework/LongRunningTask.java
+++ src/main/java/com/rishbootdev/multithreading/executor_framework/LongRunningTask.java
@@ -1,23 +1,29 @@
 package com.rishbootdev.multithreading.executor_framework;
 
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
 public class LongRunningTask implements Runnable{
 
+    private String cmd;
 
     @Override
     public void run() {
 
-        log.info("Starting the thread "+Thread.currentThread().getName());
+        log.info("Starting the thread to fulfill the command "+this.cmd+Thread.currentThread().getName());
         try{
             Thread.sleep(4000);
         }catch (InterruptedException ex) {
             log.error("Multithreading error");
         }
 
-        log.info("Ending the task");
+        log.info("Ending the task and the command got executed :{}",cmd);
     }
 
 }
